bcd32e2dba4134707433bacf038f46c430cd33b8
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2026-03-19 16:05:47 +0100

    fix auto config

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -14,10 +14,10 @@ echo "Test de connexion vers $SQL_HOST..."
 mariadb-admin ping -h"$SQL_HOST" -u"$SQL_USER" -p"$SQL_PASSWORD"
 
 # Ensuite on lance la boucle d'attente classique
-until mariadb-admin ping -h"$SQL_HOST" -u"$SQL_USER" -p"$SQL_PASSWORD" --silent; do
-    echo "Connexion échouée, nouvelle tentative..."
-    sleep 2
-done
+#until mariadb-admin ping -h"$SQL_HOST" -u"$SQL_USER" -p"$SQL_PASSWORD" --silent; do
+#    echo "Connexion échouée, nouvelle tentative..."
+#    sleep 2
+#done
 
 echo "MariaDB est prête !"
 
@@ -55,4 +55,4 @@ fi
 
 # --- 3. LANCEMENT DE PHP-FPM ---
 echo "Démarrage de PHP-FPM..."
-exec /usr/sbin/php-fpm8.2 -F
+exec /usr/sbin/php-fpm -F
