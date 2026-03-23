501f55ab944844df90c4e41163872c54e95e4562
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2026-03-23 13:56:15 +0100

    rewsf

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -39,13 +39,3 @@ echo "Démarrage du serveur MariaDB..."
 # exec mysqld_safe
 exec mariadbd --user=mysql --console
 
-#!/bin/bash
-
-service mariadb start
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-mariadb -e "CREATE USER IF NOT EXISTS '${SQL_DATABASE}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;"
-mariadb -e "FLUSH PRIVILEGES;"
-sleep 1
-service mariadb stop
-exec "$@"
